5ae4be9312b82642e34045a5d0761be2db13fd7f
Shiyam <shiyams315@gmail.com>
2023-10-30 21:31:50 +0530

    Create Reduce Method for the Action performed

--- src/app/auth/componenets/register/register.component.ts
+++ src/app/auth/componenets/register/register.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
 import { FormControl, FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { register } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'mc-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
@@ -16,10 +20,16 @@ export class RegisterComponent {
   })
 
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private store: Store) { }
 
   onSubmit() {
-    console.log('form', this.form.getRawValue());
+    // console.log('form', this.form.getRawValue());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue()
+    }
+
+    console.log(request);
+    this.store.dispatch(register({ request }))
   }
 }
 
---
+++ src/app/auth/store/reducers.ts
@@ -0,0 +1,19 @@
+import { state } from '@angular/animations';
+
+import { AuthStateInterface } from "../types/authState.interface"
+import { register } from "./actions"
+import { createFeature, createReducer, on } from "@ngrx/store"
+
+const inialState: AuthStateInterface = {
+  isSubmitting: false,
+}
+
+const authFeature = createFeature({
+  name: 'auth',
+  reducer: createReducer(
+    inialState,
+    on(register, (state) => ({ ...state, isSubmitting: true }))
+  )
+})
+
+export const { name: authFeatureKey, reducer: authReducer } = authFeature;
